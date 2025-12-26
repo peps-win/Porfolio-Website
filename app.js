df4382e970e5d88ab145bcf3d8361593990d6406
peps-win <askillman2010@gmail.com>
2025-12-26 02:47:58 -0600

    Add toggle functionality for box element

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 
 console.log("Hello, this is my website!");
 
+const button = document.getElementById("ToggleBox");
+const box = document.querySelector(".box");
+
+button.addEventListener("click", () => {
+  box.classList.toggle("active");
+});
